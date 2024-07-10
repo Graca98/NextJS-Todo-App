6dd900149dff958c5a2c0a961d90c6717c85f0fd
Grácouš <gracous1234@gmail.com>
2024-07-11 01:49:37 +0200

    Date picker, sidebar, filtry, úprava vzhledu
    Přidat input date - při psaní nového úkolu je nyní možnost zvolit datum splnění (nelze jít do minulosti). Zároven to bylo nahrazeno datumem přidání úkolu - ten stále existuje, ale ted slouží pro filtr (pro uživatele je prozatím skryt)
    
    Modal na přidání úkolu byl odstraněn. nyní se ukol přidává přes input vypadajicí jako task.
    
    Přidán sidebar, prozatím prázdný, ale funkční. Na telefonu je přes celou šířku, na menších zařízení 200px a na větších 290px.  Sidebar hamburger zůstává polohou na stejném místě.
    
    Další lehké vizuální zlepšení...

--- app/components/AddTask.jsx
+++ app/components/AddTask.jsx
@@ -1,6 +1,6 @@
 //todo Přidat lepší validaci formuláře
 
-const TaskForm = ({
+const AddTask = ({
   openTaskModal,
   setOpenTaskModal,
   formState,
@@ -19,6 +19,7 @@ const TaskForm = ({
       spanLabel: "",
       formText: "",
     });
+    setTaskDate("")
   };
 
   const handleKeyDown = (e) => {
@@ -31,12 +32,71 @@ const TaskForm = ({
   };
 
   const handleDate = (e) => {
-    setTaskDate(e.target.value);
+    setTaskDate(e.target.value)
   };
+
+  function getUSToday() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1; // getMonth() vrací měsíc od 0 do 11
+    let year = date.getFullYear();
+
+    // Přidání vedoucí nuly pro den a měsíc pokud je potřeba
+    let formattedDay = day < 10 ? `0${day}` : day;
+    let formattedMonth = month < 10 ? `0${month}` : month;
+
+    return `${year}-${formattedMonth}-${formattedDay}`; // správný formát pro HTML input date
+  }
 
   return (
     <>
-      <input
+      <label htmlFor="addTask" className={`flex justify-start cursor-pointer items-center gap-2 shadow-md p-2 bg-white w-full mb-6`}>
+        <input
+          type="checkbox"
+          className="checkbox checkbox-success checkbox-lg bg-white w-6 flex-shrink-0"
+          checked={false}
+          readOnly
+        />
+        <div className="flex flex-col lg:flex-row lg:items-center ml-1 flex-grow lg:gap-6 lg:justify-start">
+          <div className="flex flex-col lg:basis-8/12">
+            <input
+              placeholder="Přidat úkol"
+              type="text"
+              id="addTask"
+              className={`input input-solid max-w-full bg-white text-black px-0 ${formState.inputLabel}`}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setTask(e.target.value)}
+              value={task}
+            />
+            <label className="form-label px-0">
+              <span className={`form-label-alt ${formState.spanLabel}`}>
+                {formState.formText}
+              </span>
+            </label>
+          </div>
+          <input
+            type="date"
+            value={taskDate}
+            onChange={handleDate}
+            onKeyDown={handleKeyDown}
+            id="taskId"
+            min={getUSToday()}
+            className="max-w-28 lg:basis-3/12 text-gray-500 text-xs lg:text-sm"
+          ></input>
+        </div>
+        <div className="flex justify-end lg:basis-1/12">
+          <button
+            // type="button"
+            onClick={handleSubmit}
+            className="btn btn-ghost btn-md px-2"
+          >
+            Přidat
+          </button>
+        </div>
+      </label>
+
+
+      {/* <input
         className="modal-state"
         id="modal-newTask"
         type="checkbox"
@@ -94,9 +154,9 @@ const TaskForm = ({
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
 
-export default TaskForm;
+export default AddTask;
--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,7 +1,9 @@
 "use client";
 //! Není nikde použito!!
 
-export default function Sidebar({openSide, setOpenSide}) {
+import { RxHamburgerMenu } from "react-icons/rx";
+
+export default function Sidebar({ openSide, setOpenSide }) {
   // const [openSide, setOpenSide] = useState(false);
 
 
@@ -10,34 +12,25 @@ export default function Sidebar({openSide, setOpenSide}) {
       <div className="relative z-[90]">
         <div
           tabIndex="0"
-          className={`top-0 z-40 flex h-screen w-full flex-col overflow-y-auto bg-white px-3 py-1.5 focus:outline-0 sm:max-w-[290px] duration-500 ${openSide ? "block w-[290px]" : "hidden"} sm:px-6 sm:py-3`}
+          className={`${openSide ? "fixed w-full sm:relative sm:w-[200px] lg:w-[290px]" : "hidden"} top-0 z-40 flex h-screen flex-col overflow-y-auto bg-white focus:outline-0 sm:max-w-[290px] duration-500`}
         >
-          <div className="flex-1">
-            <div className="mb-2">
-              <button
-                type="button"
-                onClick={() => setOpenSide(false)}
-                id="close-topic"
-                className="absolute right-2.5 top-2.5 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-x h-5 w-5"
-                >
-                  <path d="M18 6 6 18"></path>
-                  <path d="m6 6 12 12"></path>
-                </svg>
-              </button>
+          <div className="flex-1 px-4 md:px-6">
+            <div className="flex items-center mb-2 mt-4 h-10">
+              <div className="">
+                <RxHamburgerMenu
+                  onClick={() => setOpenSide(false)}
+                  className={`${!openSide && "hidden"} text-xl`}
+                />
+              </div>
             </div>
-            <ul className="mt-6 space-y-1">
+            <div>Todo...</div>
+            <ul className="mt-4 space-y-1">
+              <li>Link 1</li>
+              <li>Link 2</li>
+              <li>Link 3</li>
+            </ul>
+            <div className="divider"></div>
+            <ul className="mt-4 space-y-1">
               <li>Link 1</li>
               <li>Link 2</li>
               <li>Link 3</li>
@@ -47,7 +40,7 @@ export default function Sidebar({openSide, setOpenSide}) {
         {/*<div
           className={`${openSide ? "fixed" : "hidden"} duration-500 inset-0 z-30 bg-gray-900 bg-opacity-50 dark:bg-opacity-80`}
         ></div>*/}
-      </div>
+      </div >
     </>
   );
 }
